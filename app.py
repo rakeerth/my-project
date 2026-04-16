8092b83376f06f0d965ba2049b8954e3b43c9322
rakeerth <rakshana2827@gmail.com>
2026-04-16 16:42:35 +0530

    testing

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-print("flower")
+import streamlit as st
 
-print("flower is beautiful")
+st.set_page_config(page_title="Simple Python Web Page", page_icon="🌐")
 
-print("but honeybee also likes it")
+st.title("Welcome")
+st.write("This is a Simple web page created using Python.")
+
+name = st.text_input("Enter your name")
+
+if st.button("Submit"):
+    if name:
+        st.success(f"Hello, {name}! Welcome to the webpage.")
+    else:
+        st.warning("Please enter your name.")

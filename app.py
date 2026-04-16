a825b646b9938117a37ba9b158c6079c4da3e441
rakeerth <rakshana2827@gmail.com>
2026-04-16 20:59:07 +0530

    Test change in streamlit appp

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.title("My Web Page")
-# ← change this line
-st.write("Hello, World! This is my updated Streamlit app.")
-st.write("Hello, World! This is my updated Streamlit app and i am testing it.")
+st.title("Weather App")
+
+# Input field
+city = st.text_input("Enter city name")
+
+# Button
+if st.button("Get Weather"):
+    if city:
+        weather = f"Weather in {city}: 🌤️ 30°C, Clear Sky"
+        st.success(weather)
+    else:
+        st.warning("Please enter a city name")

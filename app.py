8c6bd9ab3e9081650bbc364120245da2baa0d4cf
rakeerth <rakshana2827@gmail.com>
2026-04-16 20:27:23 +0530

    Test change in streamlit app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("My Web Page")
-st.write("Hello, World!")
-st.write("This is my Streamlit webpage!")
+# ← change this line
+st.write("Hello, World! This is my updated Streamlit app.")

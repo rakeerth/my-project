311f370885a35ec065283468405939b6889ba024
rakeerth <rakshana2827@gmail.com>
2026-05-05 16:25:11 +0530

    updating finnn

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ city = st.text_input("Enter city name:")
 # Button
 if st.button("Get current Weather "):
     if city:
-        weather = f"Weather in {city}: 🌤️ 30°C, Clear Skyy"
+        weather = f"Weather in {city}: 🌤️ 30°C, Clear Sky"
         st.success(weather)
     else:
-        st.warning("Enter a city name")
+        st.warning("Enter a city namee")
 st.write("New feature added here!")

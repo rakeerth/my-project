a462a2bcdb2384f6ce2625452911bd3543c2e4c3
rakeerth <rakshana2827@gmail.com>
2026-05-05 16:06:33 +0530

    updating finn

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("Weather App")
 city = st.text_input("Enter city name:")
 
 # Button
-if st.button("Get current Weatherrr "):
+if st.button("Get current Weather "):
     if city:
         weather = f"Weather in {city}: 🌤️ 30°C, Clear Sky"
         st.success(weather)
